67b1f02523294296b12b8ff0055d9f0f03baf606
okky0510 <okky.pribadi@job2go.net>
2020-03-28 14:04:42 +0700

    my First Commit (setup Master)

---
+++ SimpleNews/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleNews
+//
+//  Created by okky pribadi on 28/03/20.
+//  Copyright © 2020 okky pribadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
